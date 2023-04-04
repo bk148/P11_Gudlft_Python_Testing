1f76a6c7dffe1aa87d64a67d9987fede7160c68d
bk148 <blaise.katende@outlook.com>
2023-04-05 01:00:40 +0200

    ajout fonction "point_mis_a_jour"

--- utilis.py
+++ utilis.py
@@ -1,8 +1,22 @@
 import json
 
 
-def read(file, data_type):
+def change(file, data_type):
     with open(file) as f:
         data = json.load(f)[data_type]
 
         return data
+
+
+def points_mis_a_jour(points: str, places: str, id_club: int, id_comp=int):
+
+    with open('clubs.json') as f:
+        data = json.load(f)
+        data['clubs'][id_club]['points'] = points
+        json.dump(data, open('clubs.json', 'w'), indent=4)
+
+    with open('competitions.json') as f:
+        data = json.load(f)
+        data['competitions'][id_comp]['numberOfPlaces'] = places
+        json.dump(data, open('competitions.json', 'w'), indent=4)
+
